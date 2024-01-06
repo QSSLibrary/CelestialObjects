1ac470b80fb49901e74b061bb847b345cf85c232
RamtinKosari <ramtinkosari@gmail.com>
2024-01-07 00:26:11 +0330

    [#REFACTOR] Add Celestial Objects Header File

---
+++ CelestialObjects.hpp
@@ -0,0 +1,11 @@
+# ifndef QSSL_CELESTIAL_OBJECTS
+    /**
+     * @file CelestialObjects.hpp
+     * @author Ramtin Kosari (ramtinkosari@gmail.com)
+     * @brief QSSL Open Source Astronomy and Astrophysics Library's Celestial Objects
+     * @date 2024-01-06
+     */
+    # define QSSL_CELESTIAL_OBJECTS
+# endif // QSSL_CELESTIAL_OBJECTS
+
+
